7717397b6c9e9598bfccd28b727359491afb9c06
rwrose.projects@gmail.com <rwrose.projects@gmail.com>
2025-07-23 22:25:32 -0400

    Blaster component added. It goes bang (in the terminal :()

--- src/components/player_components/blaster_component.gd
+++ src/components/player_components/blaster_component.gd
@@ -2,12 +2,17 @@ extends Node2D
 class_name BlasterComponent
 
 func _physics_process(delta) -> void:
-    return
+	return
 
 func blast():
-    print("Blast!")
-    return
+	# Needs to get the input of the player and use that to blast. 
+	# maybe... we run this in the _physics_process with something to
+	# check for input?
+	#
+	# 
+	print("Blast!")
+	return
 
-
-
-
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("bang_input"):
+		print("Bang!")
